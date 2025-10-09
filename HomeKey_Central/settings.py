db76e8c9d99a53590a8490ec701396bc76798d59
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2025-10-08 17:32:28 -0700

    views y urls

--- HomeKey_Central/settings.py
+++ HomeKey_Central/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HomeKey_Central.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
