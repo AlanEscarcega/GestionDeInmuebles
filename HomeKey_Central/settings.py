84602e7154029154d7023838f839326cc39a9ba5
Alan Escárcega <alan.escarcegadominguez@cesunbc.edu.mx>
2025-10-31 09:20:48 -0700

    Agregadas carpetas templates/static y corregidos settings y vistas

--- HomeKey_Central/settings.py
+++ HomeKey_Central/settings.py
@@ -55,10 +55,11 @@ ROOT_URLCONF = 'HomeKey_Central.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'UrbanStart' / 'templates'],  # Django buscará aquí las plantillas base
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -66,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
 
 WSGI_APPLICATION = 'HomeKey_Central.wsgi.application'
 
